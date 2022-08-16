75479340b969958172d0699051eec4999df59ff7
kimhan1113 <kimhan1113@kakao.com>
2022-08-17 00:27:08 +0900

    add longestsubstring

---
+++ leetcode/longestsubstring.py
@@ -0,0 +1,16 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) ->int:
+        answer, left, check = 0, 0, set()
+
+        for right, ch in enumerate(s):
+            if ch not in check:
+                check.add(ch)
+                answer = max(answer, (right - left) + 1)
+            else:
+                while left <= right and ch in check:
+                    if s[left] in  check:
+                        check.remove(s[left])
+                    left += 1
+                check.add(ch)       
+
+        return answer            
